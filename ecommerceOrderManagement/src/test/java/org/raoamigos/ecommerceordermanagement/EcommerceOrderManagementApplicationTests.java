707b279180fd60938a92f0db3cf4fef043aa74d0
RishiRao123 <rishiraopuskuri@gmail.com>
2026-03-03 10:12:18 +0530

    Catalog case study completed

---
+++ ecommerceOrderManagement/src/test/java/org/raoamigos/ecommerceordermanagement/EcommerceOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.raoamigos.ecommerceordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOrderManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
